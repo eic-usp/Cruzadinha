678ee2d2b6d44a776d6f71f1c7c7110d001b7944
Maria Luíza <108702021+Mariauiza@users.noreply.github.com>
2025-05-14 16:37:27 -0300

    adicionando imagens e paineis de crédito e informações

---
+++ Assets/Scripts/PainelInfo.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PainelInfo : MonoBehaviour
+{
+   public GameObject painel;  // Referência ao painel que será controlado
+
+    // Função para mostrar o painel
+    public void MostrarPainel()
+    {
+        painel.SetActive(true);  // Torna o painel visível
+    }
+
+    // Função para fechar o painel
+    public void FecharPainel()
+    {
+        painel.SetActive(false); // Torna o painel invisível
+    }
+}
